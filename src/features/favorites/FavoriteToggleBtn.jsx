1e6ce9eef0c655d6439b83d05c81495d73950c20
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-06-21 20:43:29 +0100

    added use local storage hook

--- src/features/favorites/FavoriteToggleBtn.jsx
+++ src/features/favorites/FavoriteToggleBtn.jsx
@@ -1,106 +1,24 @@
-// "use client";
-
-// import { Star } from "lucide-react";
-// import { Toggle } from "@/components/ui/toggle";
-// import { useCurrencyParams } from "@/hooks/useCurrencyParams";
-
-// function FavoriteToggleBtn() {
-//   const { from, to, amount } = useCurrencyParams();
-
-//   function addFavorite(from, to, amount) {
-//     const favorites = JSON.parse(
-//       localStorage.getItem("favorite-currencies") || "[]",
-//     );
-
-//     const exists = favorites.some((f) => f.id === `${from}-${to}-${amount}`);
-
-//     if (exists) return;
-
-//     favorites.push({ from, to, amount, id: `${from}-${to}-${amount}` });
-
-//     localStorage.setItem("favorite-currencies", JSON.stringify(favorites));
-//   }
-
-//   function removeFavorite(from, to, amount) {
-//     const favorites = JSON.parse(
-//       localStorage.getItem("favorite-currencies") || "[]",
-//     );
-
-//     const filtered = favorites.filter(
-//       (f) => !(f.id === `${from}-${to}-${amount}`),
-//     );
-
-//     localStorage.setItem("favorite-currencies", JSON.stringify(filtered));
-//   }
-
-//   function toggleFavorite(from, to, amount) {
-//     const favorites = JSON.parse(
-//       localStorage.getItem("favorite-currencies") || "[]",
-//     );
-
-//     const exists = favorites.some((f) => f.id === `${from}-${to}-${amount}`);
-
-//     if (exists) {
-//       removeFavorite(from, to, amount);
-//     } else {
-//       addFavorite(from, to, amount);
-//     }
-//   }
-
-//   return (
-//     <Toggle
-//       aria-label="Toggle fvorite"
-//       size="lg"
-//       variant="outline"
-//       className="uppercase preset-5-medium"
-//       onPressedChange={() => toggleFavorite(from, to, amount)}
-//     >
-//       <Star className="group-aria-pressed/toggle:fill-black" />
-//       Favorite
-//     </Toggle>
-//   );
-// }
-
-// export default FavoriteToggleBtn;
-
 "use client";
 
-import { useState, useEffect } from "react";
 import { Star } from "lucide-react";
 import { Toggle } from "@/components/ui/toggle";
 import { useCurrencyParams } from "@/hooks/useCurrencyParams";
-
-function getFavorites() {
-  if (typeof window === "undefined") return [];
-  return JSON.parse(localStorage.getItem("favorite-currencies") || "[]");
-}
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 function FavoriteToggleBtn() {
   const { from, to } = useCurrencyParams();
-  const [favorites, setFavorites] = useState([]);
-
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setMounted(true);
-    setFavorites(getFavorites());
-  }, []);
+  const [favorites, setFavorites, hydrated] = useLocalStorage(
+    "favorite-currencies",
+    [],
+  );
 
   const id = `${from}-${to}`;
-  const isFavorited = mounted && favorites.some((f) => f.id === id);
+  const isFavorited = hydrated && favorites.some((f) => f.id === id);
 
   function handlePressedChange(pressed) {
-    let updated;
-
-    if (pressed) {
-      updated = [...favorites, { from, to, id }];
-    } else {
-      updated = favorites.filter((f) => f.id !== id);
-    }
-
-    setFavorites(updated);
-    localStorage.setItem("favorite-currencies", JSON.stringify(updated));
+    setFavorites((prev) =>
+      pressed ? [...prev, { from, to, id }] : prev.filter((f) => f.id !== id),
+    );
   }
 
   return (
