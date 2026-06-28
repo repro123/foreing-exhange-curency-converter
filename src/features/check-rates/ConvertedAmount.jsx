4b030e7598e7d2b79a0e40e2deb4bfe56220fb82
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-06-28 22:30:27 +0100

    feature: added aria live of polite to output element

--- src/features/check-rates/ConvertedAmount.jsx
+++ src/features/check-rates/ConvertedAmount.jsx
@@ -6,6 +6,7 @@ function ConvertedAmount({ convertedAmount }) {
   return (
     <div className="flex-1 min-w-0 overflow-x-auto no-scrollbar has-focus:outline-0 has-focus:border-primary has-focus:rounded-lg border-2 border-transparent">
       <output
+        aria-live="polite"
         htmlFor="send-amount"
         tabIndex="0"
         className="preset-1-tablet lg:preset-1 text-primary overflow-auto  w-full whitespace-nowrap "
